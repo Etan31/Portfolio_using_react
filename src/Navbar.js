50b3823dd4d16a7251ffd3aeea1a46166f7d7905
Etan31 <tumbagatristanehron31@gmail.com>
2024-07-13 22:11:21 +0800

    try

---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+   return ( 
+      <nav className="navbar">
+         <h1>Tristan Blog</h1>
+
+         <div class="links"></div>
+      </nav>
+    );
+}
+ 
+export default Navbar;
