2493f07bb6acb49c002b825e71d14e2db00097a6
Etan31 <tumbagatristanehron31@gmail.com>
2024-07-16 20:17:00 +0800

    changes on font family

--- src/Main.js
+++ src/Main.js
@@ -25,14 +25,20 @@ function Main() {
         {/* <Resume ref={resumeRef} /> */}
       
       
-      
       <main>
         < Socials />
 
         <section className="main_section">
-          <p className="paragraph highlight">Hi, my name is </p>
-          <h1 className="name">Tristan Tumbaga.</h1>
-          <h2 className="paragraph name">I build things for the web.</h2>
+          <section className="home">
+            <p className="paragraph highlight">Hi, my name is </p>
+            <h1 className="name">Tristan Tumbaga.</h1>
+            <h2 className="paragraph name">I build things for the web.</h2>
+            <p className="short-intro">I'm a software developer who specializes in developing the server-side <br/>
+            (and occasionally designing) remarkable web experiences.<br/> 
+            Currently, I'm a backend developer at <span className="highlight">xXxXxXxXxXx</span> focused on building <br/>
+            high-quality, scalable, and efficient applications.</p>
+            <button className="get-in-touch">Get In Touch</button>
+          </section>
 
                   {/* Pages */}
           <About ref={aboutRef} /> 
--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,12 +1,15 @@
+import Github_ic from './../icons/github_ic.png';
+import LinkedIn from './../icons/linkedIn_ic.png'
+
 const Socials = () => {
    return ( 
-      <aside>
+      <aside className="left">
          <a href="https://github.com/Etan31">
-            <img className="social_ic" src="./../icons/github_ic" alt="Github icon"/>
+            <img className="social_ic" src={Github_ic} alt="Github icon"/>
          </a>
 
          <a href="https://www.linkedin.com/in/tristan-tumbaga-b42478175/">
-            <img className="social_ic" src="./../icons/linkedIn_ic" alt="LinkedIn icon" />
+            <img className="social_ic" src={LinkedIn} alt="LinkedIn icon" />
          </a>
           <div className="vertical"></div>
       </aside>
