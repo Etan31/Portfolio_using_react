efa755d14b46f99b3319fc50a46be6d9fa30b2c7
Etan31 <tumbagatristanehron31@gmail.com>
2024-07-31 23:46:40 +0800

    added tech icon

--- src/Projects.js
+++ src/Projects.js
@@ -4,23 +4,9 @@ const Project = React.forwardRef((props, ref) => {
   return (
     <section className="Projects" ref={ref}>
       <h1>hello Project</h1>
-        <ul className="featured-projects">
+      <ul className="featured-projects">
+
         {/* project 1 */}
-        <li className="projects">
-          <div className="project-content">
-            <div className="content-wrapper">
-              <p className="project-overline">Featured Project</p>
-              <h3 className="project-title">SchedBin</h3>
-              <div className="project-description"></div>
-              <ul className="project-tech-list"></ul>
-              <div className="project-links"></div>
-            </div>
-          </div>
-          <div className="project-image"></div>
-        </li>
-
-
-        {/* project 2 */}
         <li className="projects">
           <div className="project-content">
             <div className="content-wrapper">
@@ -34,8 +20,7 @@ const Project = React.forwardRef((props, ref) => {
           <div className="project-image"></div>
         </li>
 
-
-        {/* project 3 */}
+        {/* project 2 */}
         <li className="projects">
           <div className="project-content">
             <div className="content-wrapper">
@@ -49,8 +34,33 @@ const Project = React.forwardRef((props, ref) => {
           <div className="project-image"></div>
         </li>
 
+        {/* project 3 */}
+        <li className="projects">
+          <div className="project-content">
+            <div className="content-wrapper">
+              <p className="project-overline">Featured Project</p>
+              <h3 className="project-title">SchedBin</h3>
+              <div className="project-description"></div>
+              <ul className="project-tech-list"></ul>
+              <div className="project-links">
+                <a target="_blank" rel="noreferrer"
+                  href="https://github.com/Etan31/SchedBin-Scheduler-for-Garbage-Collection-">
+                  <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                    className="feather feather-github">
+                    <title>GitHub</title>
+                    <path
+                      d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
+                    </path>
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="project-image"></div>
+        </li>
 
-        {/* project 4 ///   Ongoing*/}
+        {/* project 4 /// Ongoing*/}
         <li className="projects">
           <div className="project-content">
             <div className="content-wrapper">
@@ -63,6 +73,7 @@ const Project = React.forwardRef((props, ref) => {
           </div>
           <div className="project-image"></div>
         </li>
+
       </ul>
     </section>
   );
