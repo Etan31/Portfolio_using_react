91f1a8e496893dc98b6cc476862e1be719cf3170
Etan31 <tumbagatristanehron31@gmail.com>
2024-07-31 23:14:22 +0800

    moved projects from contact to projects.js

--- src/Contact.js
+++ src/Contact.js
@@ -4,66 +4,6 @@ const Contact = React.forwardRef((props, ref) => {
   return (
     <section className="Contact" ref={ref}>
       <h2 className="numbered-heading" >Some Things I’ve Built</h2>
-      <ul className="featured-projects">
-        {/* project 1 */}
-        <li className="projects">
-          <div className="project-content">
-            <div className="content-wrapper">
-              <p className="project-overline">Featured Project</p>
-              <h3 className="project-title">SchedBin</h3>
-              <div className="project-description"></div>
-              <ul className="project-tech-list"></ul>
-              <div className="project-links"></div>
-            </div>
-          </div>
-          <div className="project-image"></div>
-        </li>
-
-
-        {/* project 2 */}
-        <li className="projects">
-          <div className="project-content">
-            <div className="content-wrapper">
-              <p className="project-overline">Featured Project</p>
-              <h3 className="project-title">Skycast</h3>
-              <div className="project-description"></div>
-              <ul className="project-tech-list"></ul>
-              <div className="project-links"></div>
-            </div>
-          </div>
-          <div className="project-image"></div>
-        </li>
-
-
-        {/* project 3 */}
-        <li className="projects">
-          <div className="project-content">
-            <div className="content-wrapper">
-              <p className="project-overline">Featured Project</p>
-              <h3 className="project-title">Speech2Text</h3>
-              <div className="project-description"></div>
-              <ul className="project-tech-list"></ul>
-              <div className="project-links"></div>
-            </div>
-          </div>
-          <div className="project-image"></div>
-        </li>
-
-
-        {/* project 4 ///   Ongoing*/}
-        <li className="projects">
-          <div className="project-content">
-            <div className="content-wrapper">
-              <p className="project-overline">Featured Project</p>
-              <h3 className="project-title">TrackRecord</h3>
-              <div className="project-description"></div>
-              <ul className="project-tech-list"></ul>
-              <div className="project-links"></div>
-            </div>
-          </div>
-          <div className="project-image"></div>
-        </li>
-      </ul>
     </section>
   );
 });
