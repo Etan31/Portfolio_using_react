7a70c9de00b9649fbeab9ce97efd771c6f0bbf26
Etan31 <tumbagatristanehron31@gmail.com>
2024-08-07 21:34:28 +0800

    sdf2

--- src/Projects.js
+++ src/Projects.js
@@ -196,11 +196,23 @@ const Project = React.forwardRef((props, ref) => {
                  Able to save and transfer your progress through your Google account(s).
               </div>
               <ul className="project-tech-list"></ul>
-              <div className="project-links"></div>
+              <div className="project-links">
+                <a target="_blank" rel="noreferrer"
+                  href="https://github.com/Etan31/TrackRecord">
+                  <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" strokeWidth="2" height="24px" width="24px"  strokeLinecap="round" strokeLinejoin="round"
+                    className="feather feather-github">
+                    <title>GitHub</title>
+                    <path
+                      d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
+                    </path>
+                  </svg>
+                </a>
+              </div>
             </div>
           </div>
-          <div className="project-image">
-            <div class="windowPreview">
+          <div className="project-image img-container1">
+            <div class="windowPreview img-wrapper1">
               <img src={trackRecord} ref={trackRecordRef} className="imgPreview stt1" alt="Speech-to-text img preview"/>
             </div>
           </div>
