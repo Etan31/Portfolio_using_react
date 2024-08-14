626208cedb05d7cdde3e108d8930b8a8071b7dd7
Etan31 <tumbagatristanehron31@gmail.com>
2024-08-14 21:57:05 +0800

    Testimonial - removed h1

--- src/Testimonials.js
+++ src/Testimonials.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Testimonials = React.forwardRef((props, ref) => {
   return (
     <section className="Testimonials" ref={ref}>
-      <h1>hello Testimonials</h1>
+      
       <div class="wrapper">
         <div class="container"></div>
       </div>
