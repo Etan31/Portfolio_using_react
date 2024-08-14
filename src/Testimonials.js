21bbcd0c325b111563e658ccee3328c35ee5cad5
Etan31 <tumbagatristanehron31@gmail.com>
2024-08-14 21:53:55 +0800

    Testimonial- added wrapper and container

--- src/Testimonials.js
+++ src/Testimonials.js
@@ -3,8 +3,16 @@ import React from 'react';
 const Testimonials = React.forwardRef((props, ref) => {
   return (
     <section className="Testimonials" ref={ref}>
-      {/* ... */}
       <h1>hello Testimonials</h1>
+      <div class="wrapper">
+        <div class="container"></div>
+      </div>
+      <div class="wrapper">
+        <div class="container"></div>
+      </div>
+      <div class="wrapper">
+        <div class="container"></div>
+      </div>
     </section>
   );
 });
