e4fc59c01d556f094f0ae0e844bc5ca94d937fb6
Etan31 <tumbagatristanehron31@gmail.com>
2024-08-15 22:56:31 +0800

    restructure

--- src/Testimonials.js
+++ src/Testimonials.js
@@ -4,14 +4,24 @@ const Testimonials = React.forwardRef((props, ref) => {
   return (
     <section className="Testimonials" ref={ref}>
       
-      <div class="wrapper">
-        <div class="container"></div>
-      </div>
-      <div class="wrapper">
-        <div class="container"></div>
-      </div>
-      <div class="wrapper">
-        <div class="container"></div>
+      <div class="testimony-container">
+        <div class="wrapper">
+          {/* middle  user-info*/}
+            <div class="user-info">
+              <img src="" alt="" class="img"></img>
+              <div class="user-occupation"></div>
+              <div class="user-name"></div>
+            </div>
+            <p class="testimony-p"></p>
+        </div>
+        <div class="wrapper">
+            <div class="user-info">
+              <img src="" alt="" class="img"></img>
+              <div class="user-occupation"></div>
+              <div class="user-name"></div>
+            </div>
+            <p class="testimony-p"></p>
+        </div>
       </div>
     </section>
   );
