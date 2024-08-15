e9902a2de95c0b2b72af34fb7caa0e4879afb3e2
Etan31 <tumbagatristanehron31@gmail.com>
2024-08-15 23:02:11 +0800

    testimony- profession

--- src/Testimonials.js
+++ src/Testimonials.js
@@ -10,7 +10,7 @@ const Testimonials = React.forwardRef((props, ref) => {
             <div class="user-info">
               <img src="" alt="" class="img"></img>
               <div class="user-name">Sample A. Username</div>
-              <div class="user-occupation"></div>
+              <div class="user-profession">Profession ABC</div>
             </div>
             <p class="testimony-p">Lorem ipsum dolor sit amet consectetur adipisicing elit.  </p>
         </div>
@@ -18,7 +18,7 @@ const Testimonials = React.forwardRef((props, ref) => {
             <div class="user-info">
               <img src="" alt="" class="img"></img>
               <div class="user-name">Sample B. Username</div>
-              <div class="user-occupation"></div>
+              <div class="user-profession">Profession ABC</div>
             </div>
             <p class="testimony-p">Lorem ipsum dolor sit amet consectetur adipisicing elit.  </p>
         </div>
