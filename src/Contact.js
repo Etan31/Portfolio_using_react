08c0c27208041e5ceb9a5b6db99e17433afcb8cb
Etan31 <tumbagatristanehron31@gmail.com>
2024-08-17 14:00:49 +0800

    contact - changed header

--- src/Contact.js
+++ src/Contact.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Contact = React.forwardRef((props, ref) => {
   return (
     <section className="Contact" ref={ref}>
-      <h2 className="numbered-heading" >What’s Next?</h2>
+      <h2 className="numbered-heading" >Get in Touch</h2>
       <h3> Secret  :) </h3>
     </section>
   );
