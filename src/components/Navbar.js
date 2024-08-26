51f1a892d12047b9b1a256a88364ed0938fe561f
Etan31 <tumbagatristanehron31@gmail.com>
2024-08-27 00:57:52 +0800

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,49 +17,38 @@ const Navbar = ({ aboutRef, projectsRef, testimonialsRef, contactRef }) => {
 
   return (
     <nav className="sideNav">
-      <h1 className="portfolio-name start">
-        <MyLogo className="t-logo" />
-        
-        
-      </h1>
-      <ol className="navList">
-        <li className="navMenu" onClick={() => handleClick(aboutRef)}>About</li>
-        <li className="navMenu" onClick={() => handleClick(projectsRef)}>Projects</li>
-        <li className="navMenu" onClick={() => handleClick(testimonialsRef)}>Testimonials</li>
-        <li className="navMenu" onClick={() => handleClick(contactRef)}>Contact</li>
-        <div className="resumebtn-div">
-          <a
-            className="resume-button"
-            href="/Resume.pdf"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Resume
-          </a>
-        </div>
-      </ol>
-      <div className="side-menu">
-        {/* <img src={Ic_Menu} alt="Menu" /> */}
-        <img  src={Ic_Menu} className="menuBtn" alt="Menu"  onClick={onClickMenu}/>
+    <h1 className="portfolio-name start">
+      <MyLogo className="t-logo" />
+
+    </h1>
+    <ol className="navList">
+      <li className="navMenu" onClick={()=> handleClick(aboutRef)}>About</li>
+      <li className="navMenu" onClick={()=> handleClick(projectsRef)}>Projects</li>
+      <li className="navMenu" onClick={()=> handleClick(testimonialsRef)}>Testimonials</li>
+      <li className="navMenu" onClick={()=> handleClick(contactRef)}>Contact</li>
+      <div className="resumebtn-div">
+        <a className="resume-button" href="/Resume.pdf" rel="noopener noreferrer" target="_blank">
+          Resume
+        </a>
       </div>
-            {/* Side menu content */}
-      <div className={`sideMenuContent ${isMenuOpen ? 'open' : ''}`}>
-                <li className="navMenu" onClick={() => handleClick(aboutRef)}>About</li>
-        <li className="navMenu" onClick={() => handleClick(projectsRef)}>Projects</li>
-        <li className="navMenu" onClick={() => handleClick(testimonialsRef)}>Testimonials</li>
-        <li className="navMenu" onClick={() => handleClick(contactRef)}>Contact</li>
-        <div className="resumebtn-div">
-          <a
-            className="resume-button"
-            href="/Resume.pdf"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Resume
-          </a>
-        </div>
+    </ol>
+    <div className="side-menu">
+      {/* <img src={Ic_Menu} alt="Menu" /> */}
+      <img src={Ic_Menu} className="menuBtn" alt="Menu" onClick={onClickMenu} />
+    </div>
+    {/* Side menu content */}
+    {/* <div className={`sideMenuContent ${isMenuOpen ? 'open' : '' }`}>
+      <li className="navMenu" onClick={()=> handleClick(aboutRef)}>About</li>
+      <li className="navMenu" onClick={()=> handleClick(projectsRef)}>Projects</li>
+      <li className="navMenu" onClick={()=> handleClick(testimonialsRef)}>Testimonials</li>
+      <li className="navMenu" onClick={()=> handleClick(contactRef)}>Contact</li>
+      <div className="resumebtn-div">
+        <a className="resume-button" href="/Resume.pdf" rel="noopener noreferrer" target="_blank">
+          Resume
+        </a>
       </div>
-    </nav>
+    </div> */}
+  </nav>
   );
 }
 
