bc33e2640da680c645089ad602fc9232193eb63c
Etan31 <tumbagatristanehron31@gmail.com>
2024-08-27 00:29:17 +0800

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,28 @@
 // import MyLogo from './../icons/myLogo.svg';
 import MyLogo from './../components/Logo';
+import React, {useState} from 'react';
+
+import Ic_Menu from './../icons/ic_menu.jpg';
 
 const Navbar = ({ aboutRef, projectsRef, testimonialsRef, contactRef }) => {
   const handleClick = (ref) => {
     ref.current.scrollIntoView({ behavior: 'smooth' });
   };
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const onClickMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
-    <nav>
+    <nav className="sideNav">
       <h1 className="portfolio-name start">
         <MyLogo className="t-logo" />
         
         
       </h1>
-      <ol>
+      <ol className="navList">
         <li className="navMenu" onClick={() => handleClick(aboutRef)}>About</li>
         <li className="navMenu" onClick={() => handleClick(projectsRef)}>Projects</li>
         <li className="navMenu" onClick={() => handleClick(testimonialsRef)}>Testimonials</li>
@@ -29,6 +38,27 @@ const Navbar = ({ aboutRef, projectsRef, testimonialsRef, contactRef }) => {
           </a>
         </div>
       </ol>
+      <div className="side-menu">
+        {/* <img src={Ic_Menu} alt="Menu" /> */}
+        <img  src={Ic_Menu} className="menuBtn" alt="Menu"  onClick={onClickMenu}/>
+      </div>
+            {/* Side menu content */}
+      <div className={`sideMenuContent ${isMenuOpen ? 'open' : ''}`}>
+                <li className="navMenu" onClick={() => handleClick(aboutRef)}>About</li>
+        <li className="navMenu" onClick={() => handleClick(projectsRef)}>Projects</li>
+        <li className="navMenu" onClick={() => handleClick(testimonialsRef)}>Testimonials</li>
+        <li className="navMenu" onClick={() => handleClick(contactRef)}>Contact</li>
+        <div className="resumebtn-div">
+          <a
+            className="resume-button"
+            href="/Resume.pdf"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Resume
+          </a>
+        </div>
+      </div>
     </nav>
   );
 }
