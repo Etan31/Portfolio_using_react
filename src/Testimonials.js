7e5926cf9a28b4c7b140016cb45203414218779f
Etan31 <tumbagatristanehron31@gmail.com>
2024-09-05 19:29:33 +0800

    Added Testimony

--- src/Testimonials.js
+++ src/Testimonials.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './css/testimony.css';
-import sampleImg from './imgs/sample-img.jpg';
+import sampleImg from './imgs/profiles/sample-img.jpg';
+import Armand from './imgs/profiles/Armand.jpg'
 
 const Testimonials = React.forwardRef((props, ref) => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -34,17 +35,21 @@ const Testimonials = React.forwardRef((props, ref) => {
       <div className="testimony-container">
         <div className="carousel" ref={carouselRef}>
           <div className="testimony-wrapper">
-            <img src={sampleImg} alt="sample profile" className="img-profile"></img>
-            <div className="user-name">Sample A. Username</div>
-            <div className="user-profession">Profession ABC</div>
+            <img src={Armand} alt="sample profile" className="img-profile"></img>
+            <div className="user-name"><span className="highlight">Armand Ralph Anacta</span></div>
+            <div className="user-profession">QCBOT WEB DEVELOPER</div>
             <p className="testimony-p">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos dolores voluptatum exercitationem nostrum ad
-              itaque cupiditate.
+              Tristan Ehron consistently delivers outstanding web solutions, demonstrating both skill and innovation. 
+              Whether it's complex applications or optimizing performance, his expertise is evident.
+              Working with Tristan has always been a smooth and enjoyable experience. 
+              I highly recommend him for anyone in need of top-quality web development services.
             </p>
           </div>
           <div className="testimony-wrapper">
             <img src={sampleImg} alt="sample profile" className="img-profile"></img>
-            <div className="user-name">Sample B. Username</div>
+            <div className="user-name">
+              <span className="highlight">Sample B. Username</span>
+            </div>
             <div className="user-profession">Profession ABC</div>
             <p className="testimony-p">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos dolores voluptatum exercitationem nostrum ad
@@ -53,7 +58,9 @@ const Testimonials = React.forwardRef((props, ref) => {
           </div>
           <div className="testimony-wrapper">
             <img src={sampleImg} alt="sample profile" className="img-profile"></img>
-            <div className="user-name">Sample C. Username</div>
+            <div className="user-name">
+              <span className="highlight">Sample C. Username</span>
+            </div>
             <div className="user-profession">Profession ABC</div>
             <p className="testimony-p">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos dolores voluptatum exercitationem nostrum ad
