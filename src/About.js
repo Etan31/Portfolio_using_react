9d02d29937adb1ec5d688ff3e2d808855f61d6fd
Etan31 <tumbagatristanehron31@gmail.com>
2024-09-24 11:24:19 +0800

    dubgging useAnimation

--- src/About.js
+++ src/About.js
@@ -8,17 +8,17 @@ const About = React.forwardRef((props, ref) => {
       <div className="inner">
         <div className="about-me-text">
           <div className="paragaraph">
-            <p>
+            <p className="me">
               Hello! My name is Tristan Ehron Tumbaga, and I enjoy creating things that live on
               the internet. My interest in web development began when I chose to explore the world of online technologies;
               playing with various projects taught me a lot about HTML, CSS, and JavaScript!
             </p>
-            <p>
+            <p className="me">
               Fast forward to now, and I've had the opportunity to work for <span className="highlight"><a rel="noopener noreferrer" target="_blank" className="highlight-link" href="https://web.facebook.com/dostpstceasternsamar/">Bytewise Information Technology Solutions</a></span> and
               <span className="highlight"> <a className="highlight-link" rel="noopener noreferrer" target="_blank" href="https://web.facebook.com/profile.php?id=61554203821069">DOST PSTO-Eastern Samar</a></span> in Borongan, Eastern Samar. These days, my major focus is on developing
               user-friendly products and experiences.
             </p>
-            <p>
+            <p className="me">
               While I am not currently employed, I'm constantly developing my skills and working on projects to keep up
               with the latest technology advancements.
             </p>
